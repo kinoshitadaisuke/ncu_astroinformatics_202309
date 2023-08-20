8382c8162b2077f28d22bf3ba4ebe950af2fa9e5
Daisuke <mhdf23888@gmail.com>
2023-08-20 13:09:59 +0800

    modifying s01/ai2023_s01_00_00.py

--- s01/ai2023_s01_00_00.py
+++ s01/ai2023_s01_00_00.py
@@ -1,8 +1,8 @@
 #!/usr/pkg/bin/python3.10
 
 #
-# Time-stamp: <2023/08/20 10:23:01 (CST) daisuke>
+# Time-stamp: <2023/08/20 13:09:25 (CST) daisuke>
 #
 
 # printing a sentence using print () function
-print (f'This is my very first Python script for course "Astroinformatics"!')
+print ('This is my very first Python script for the course "Astroinformatics"!')
