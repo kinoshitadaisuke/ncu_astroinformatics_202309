d825a93b3d6e343afb1afaa44b8800dbae506a6d
Daisuke <mhdf23888@gmail.com>
2023-09-06 07:18:02 +0800

    adding s00/ai2023_s00_00_00.py

---
+++ s00/ai2023_s00_00_00.py
@@ -0,0 +1,8 @@
+#!/usr/pkg/bin/python3.10
+
+#
+# Time-stamp: <2023/09/06 07:17:18 (CST) daisuke>
+#
+
+# a sample code for a test to run a Python script
+print ('This is my very first Python script for the course "Astroinformatics"!')
