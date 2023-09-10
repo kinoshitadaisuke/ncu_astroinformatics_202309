de723586da982519b668e07b5f6aabaa5df8d8cc
Daisuke <mhdf23888@gmail.com>
2023-09-10 16:18:41 +0800

    adding sample Python scripts for the session 01

--- s01/ai2023_s01_00_00.py
+++ s01/ai2023_s01_00_00.py
@@ -1,8 +1,8 @@
 #!/usr/pkg/bin/python3.10
 
 #
-# Time-stamp: <2023/08/20 13:09:25 (CST) daisuke>
+# Time-stamp: <2023/09/10 13:57:12 (CST) daisuke>
 #
 
 # printing a sentence using print () function
-print ('This is my very first Python script for the course "Astroinformatics"!')
+print ('I love astronomy! I love Python!')
