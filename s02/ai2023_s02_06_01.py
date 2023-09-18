9da23fc59597cfcd2740a159ef798a87840e8aa3
Daisuke <mhdf23888@gmail.com>
2023-09-18 15:25:40 +0800

    modifying s02/ai2023_s02_06_01.py

--- s02/ai2023_s02_06_01.py
+++ s02/ai2023_s02_06_01.py
@@ -1,18 +1,19 @@
 #!/usr/pkg/bin/python3.10
 
 #
-# Time-stamp: <2023/09/17 15:53:09 (CST) daisuke>
+# Time-stamp: <2023/09/18 15:24:36 (CST) daisuke>
 #
 
 # importing subprocess module
 import subprocess
 
-# executing a command "date -u -r 1700000000" and capturing output
-result = subprocess.run ('date -u -r 1700000000', \
+# executing a command "ls -lF /" and capturing output
+result = subprocess.run ('ls -lF /', \
                          shell=True, capture_output=True)
 
 # stdout of command execution
 output = result.stdout.decode ('utf-8')
 
 # printing result of command execution
-print (f'1.7 * 10^9 second from 01/Jan/1970 = {output}')
+print (f'Files and directories at root directory:')
+print (f'{output}')
