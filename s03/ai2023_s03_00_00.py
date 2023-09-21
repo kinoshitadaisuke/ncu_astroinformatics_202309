626b021663b2c0599354770afb7ba3b3331a3255
Daisuke <mhdf23888@gmail.com>
2023-09-21 21:17:31 +0800

    adding s03/ai2023_s03_00_00.py

---
+++ s03/ai2023_s03_00_00.py
@@ -0,0 +1,14 @@
+#!/usr/pkg/bin/python3.10
+
+#
+# Time-stamp: <2023/09/21 21:15:55 (CST) daisuke>
+#
+
+# importing Numpy module
+import numpy
+
+# making a Numpy array (ndarray)
+array_a = numpy.array ([0.1, 2.3, 4.5, 6.7, 8.9])
+
+# printing Numpy array
+print (f'array_a = {array_a}')
