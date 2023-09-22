235806e9d4ed8903a1be55b49dc42ef2c4833dc4
Daisuke <mhdf23888@gmail.com>
2023-09-22 10:13:18 +0800

    adding s03/ai2023_s03_00_01.py

---
+++ s03/ai2023_s03_00_01.py
@@ -0,0 +1,17 @@
+#!/usr/pkg/bin/python3.10
+
+#
+# Time-stamp: <2023/09/21 21:38:24 (CST) daisuke>
+#
+
+# importing Numpy module
+import numpy
+
+# making a Numpy array (ndarray)
+array_a = numpy.array ([0.1, 2.3, 4.5, 6.7, 8.9])
+
+# type of "array_a"
+type_array_a = type (array_a)
+
+# printing type of "array_a"
+print (f'type of "array_a" = {type_array_a}')
