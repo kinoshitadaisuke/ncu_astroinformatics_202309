5757162383ea28adccdb00461fcffad78c419112
Daisuke <mhdf23888@gmail.com>
2023-10-04 15:13:25 +0800

    adding s05/ai2023_s05_00_00.py

---
+++ s05/ai2023_s05_00_00.py
@@ -0,0 +1,14 @@
+#!/usr/pkg/bin/python3.10
+
+#
+# Time-stamp: <2023/10/04 15:02:20 (CST) daisuke>
+#
+
+# importing scipy module
+import scipy.constants
+
+# value of pi
+pi = scipy.constants.pi
+
+# printing value of pi
+print (f'pi = {pi}')
