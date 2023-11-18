df45f1e3b1afb0bbd4b240b79f20ea36b6518339
kinoshitadaisuke <mhdf23888@gmail.com>
2023-11-18 20:40:27 +0800

    adding sample scripts for the session 10

---
+++ s10/ai2023_s10_03_01.py
@@ -0,0 +1,42 @@
+#!/usr/pkg/bin/python3.10
+
+#
+# Time-stamp: <2023/11/18 20:24:58 (Taiwan_Standard_Time_UT+8) daisuke>
+#
+
+# importing urllib module
+import urllib.request
+
+# importing ssl module
+import ssl
+
+# allow insecure downloading
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# URL of data file
+url_data = 'https://relay.sao.ru/lv/lvgdb/tables/lvg_table6.dat'
+
+# output file name
+file_output = 'lvg_d.data'
+
+# printing status
+print (f'Now, fetching {url_data}...')
+
+# opening URL
+with urllib.request.urlopen (url_data) as fh_read:
+    # reading data
+    data_byte = fh_read.read ()
+
+# printing status
+print (f'Finished fetching {url_data}!')
+
+# printing status
+print (f'Now, writing data into file "{file_output}"...')
+
+# opening file for writing
+with open (file_output, 'wb') as fh_write:
+    # writing data
+    fh_write.write (data_byte)
+
+# printing status
+print (f'Finished writing data into file "{file_output}"!')
