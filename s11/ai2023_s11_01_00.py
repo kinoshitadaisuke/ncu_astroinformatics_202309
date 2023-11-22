3ecc6c896050c1eb7dee99498c111591d795d5ac
kinoshitadaisuke <mhdf23888@gmail.com>
2023-11-22 15:06:38 +0800

    adding sample scripts for the session 11

---
+++ s11/ai2023_s11_01_00.py
@@ -0,0 +1,43 @@
+#!/usr/pkg/bin/python3.10
+
+#
+# Time-stamp: <2023/11/22 14:40:53 (Taiwan_Standard_Time_UT+8) daisuke>
+#
+
+# importing astropy module
+import astropy.units
+import astropy.coordinates
+
+# importing astroquery module
+import astroquery.simbad
+
+# units
+u_ha  = astropy.units.hourangle
+u_deg = astropy.units.deg
+
+# target object name
+target = "Sirius"
+
+# name resolver
+query_result = astroquery.simbad.Simbad.query_object (target)
+
+# coordinate from Simbad
+ra_str  = query_result['RA'][0]
+dec_str = query_result['DEC'][0]
+
+# using skycoord of astropy
+coord = astropy.coordinates.SkyCoord (ra_str, dec_str, frame='icrs', \
+                                      unit=(u_ha, u_deg) )
+
+# coordinate in decimal degree
+ra_deg  = coord.ra.deg
+dec_deg = coord.dec.deg
+
+# coordinate in hms and dms format
+ra_hms  = coord.ra.to_string (u_ha)
+dec_dms = coord.dec.to_string (u_deg, alwayssign=True)
+
+# printing result
+print (f"target: {target}")
+print (f" RA  = {ra_hms:>14s} = {ra_deg:10.6f} deg")
+print (f" Dec = {dec_dms:>14s} = {dec_deg:+10.6f} deg")
