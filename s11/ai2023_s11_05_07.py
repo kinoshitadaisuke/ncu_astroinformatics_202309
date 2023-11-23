04e390840d9ae8bbfaa5cd72332e1800b231e422
kinoshitadaisuke <mhdf23888@gmail.com>
2023-11-23 22:43:38 +0800

    adding sample scripts for the session 11

---
+++ s11/ai2023_s11_05_07.py
@@ -0,0 +1,85 @@
+#!/usr/pkg/bin/python3.10
+
+#
+# Time-stamp: <2023/11/23 12:39:09 (Taiwan_Standard_Time_UT+8) daisuke>
+#
+
+# importing argparse module
+import argparse
+
+# importing numpy module
+import numpy
+
+# importing astropy module
+import astropy.io.votable
+
+# importing matplotlib module
+import matplotlib.figure
+import matplotlib.backends.backend_agg
+
+# construction of parser object for argparse
+descr  = 'SNR of b-band magnitude measurements of Gaia DR3 data'
+parser = argparse.ArgumentParser (description=descr)
+
+# adding arguments
+parser.add_argument ('-i', '--input', help='input VOTable file name')
+parser.add_argument ('-o', '--output', help='output figure file name')
+parser.add_argument ('-t', '--title', help='title of plot')
+parser.add_argument ('-r', '--resolution', type=float, default=225.0, \
+                     help='resolution in DPI (default: 225)')
+
+# command-line argument analysis
+args = parser.parse_args ()
+
+# input parameters
+file_votable   = args.input
+file_fig       = args.output
+title          = args.title
+resolution_dpi = args.resolution
+
+# reading VOTable
+table = astropy.io.votable.parse_single_table (file_votable).to_table ()
+
+# data
+data_id        = numpy.array (table['source_id'])
+data_ra        = numpy.array (table['ra'])
+data_dec       = numpy.array (table['dec'])
+data_parallax  = numpy.array (table['parallax'])
+data_pmra      = numpy.array (table['pmra'])
+data_pmdec     = numpy.array (table['pmdec'])
+data_rv        = numpy.array (table['radial_velocity'])
+data_b         = numpy.array (table['phot_bp_mean_mag'])
+data_g         = numpy.array (table['phot_g_mean_mag'])
+data_r         = numpy.array (table['phot_rp_mean_mag'])
+data_br        = numpy.array (table['bp_rp'])
+data_bg        = numpy.array (table['bp_g'])
+data_gr        = numpy.array (table['g_rp'])
+data_ra_err    = numpy.array (table['ra_error'])
+data_dec_err   = numpy.array (table['dec_error'])
+data_pmra_err  = numpy.array (table['pmra_error'])
+data_pmdec_err = numpy.array (table['pmdec_error'])
+data_p_snr     = numpy.array (table['parallax_over_error'])
+data_b_snr     = numpy.array (table['phot_bp_mean_flux_over_error'])
+data_g_snr     = numpy.array (table['phot_g_mean_flux_over_error'])
+data_r_snr     = numpy.array (table['phot_rp_mean_flux_over_error'])
+
+# x range
+x_range = (0.0, 50.0)
+
+# making objects "fig", "canvas", and "ax"
+fig    = matplotlib.figure.Figure ()
+canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
+ax     = fig.add_subplot (111)
+
+# axes
+ax.set_xlabel ('SNR of g-band photometric measurements')
+ax.set_ylabel ('Number of Stars')
+ax.grid ()
+ax.set_title (title)
+ax.set_xlim (x_range)
+
+# plotting errors
+ax.hist (data_b_snr, bins=300, range=x_range )
+
+# saving file
+fig.savefig (file_fig, dpi=resolution_dpi, bbox_inches="tight")
