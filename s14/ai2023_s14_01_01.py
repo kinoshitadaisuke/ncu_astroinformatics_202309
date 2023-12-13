c21b2041f351e46a0034de7b8a90c8634e07122a
kinoshitadaisuke <mhdf23888@gmail.com>
2023-12-13 16:39:45 +0800

    adding sample scripts for the sesseion 14

---
+++ s14/ai2023_s14_01_01.py
@@ -0,0 +1,35 @@
+#!/usr/pkg/bin/python3.10
+
+#
+# Time-stamp: <2023/12/13 15:28:38 (Taiwan_Standard_Time_UT+8) daisuke>
+#
+
+# importing astropy module
+import astropy.time
+
+# importing astroquery module
+import astroquery.jplhorizons
+
+# target name
+target = 'Sun'
+
+# observatory (D35 ==> Lulin Observatory)
+#
+# To get a list of available observatory codes, visit following web page.
+#   https://www.minorplanetcenter.net/iau/lists/ObsCodes.html
+#
+obs_site = 'D35'
+
+# date/time
+t  = astropy.time.Time ('2024-01-01T04:00:00', scale='utc', format='isot')
+jd = t.jd
+
+# sending a query to NASA/JPL Horizons system
+obj = astroquery.jplhorizons.Horizons (id_type=None, id=target, \
+                                       location=obs_site, epochs=jd)
+
+# getting ephemerides
+ephemerides = obj.ephemerides ()
+
+# printing ephemerides
+print (f'type (ephemerides) = {type (ephemerides)}')
