aeeb333ba196c2aa2fa5393e955c5dd952ad41c2
kinoshitadaisuke <mhdf23888@gmail.com>
2023-12-15 10:13:30 +0800

    adding sample scripts for the session 12

---
+++ s14/ai2023_s14_04_03.py
@@ -0,0 +1,25 @@
+#!/usr/pkg/bin/python3.10
+
+#
+# Time-stamp: <2023/12/14 20:10:52 (Taiwan_Standard_Time_UT+8) daisuke>
+#
+
+# importing subprocess module
+import subprocess
+
+# ffmpeg command
+ffmpeg = 'ffmpeg'
+
+# ffmpeg options
+options_ffmpeg = f'-f image2 -start_number 0 -framerate 30' \
+    + f' -i binary/binary_%08d.png' \
+    + f' -an -vcodec libx264 -pix_fmt yuv420p -threads 4'
+
+# output file name
+file_output = 'binary.mp4'
+
+# command to be executed
+command_ffmpeg = f'{ffmpeg} {options_ffmpeg} {file_output}'
+
+# execution of command
+subprocess.run (command_ffmpeg, shell=True)
