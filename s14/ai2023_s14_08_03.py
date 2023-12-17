fbccb71b93ea89a7bd53aa75a57b90513a993629
kinoshitadaisuke <mhdf23888@gmail.com>
2023-12-17 18:47:01 +0800

    adding sample scripts for the session 14

---
+++ s14/ai2023_s14_08_03.py
@@ -0,0 +1,25 @@
+#!/usr/pkg/bin/python3.10
+
+#
+# Time-stamp: <2023/12/17 12:18:38 (Taiwan_Standard_Time_UT+8) daisuke>
+#
+
+# importing subprocess module
+import subprocess
+
+# ffmpeg command
+ffmpeg = 'ffmpeg6'
+
+# ffmpeg options
+options_ffmpeg = f'-f image2 -start_number 0 -framerate 30' \
+    + f' -i iss3d_cropped/iss3d_%06dc.png' \
+    + f' -an -vcodec libx264 -pix_fmt yuv420p -threads 4'
+
+# output file name
+file_output = 'iss3d.mp4'
+
+# command to be executed
+command_ffmpeg = f'{ffmpeg} {options_ffmpeg} {file_output}'
+
+# execution of command
+subprocess.run (command_ffmpeg, shell=True)
