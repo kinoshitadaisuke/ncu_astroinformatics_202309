c08ac3e75dd15d101cc65b3d1b9fdc555e2d9024
kinoshitadaisuke <mhdf23888@gmail.com>
2023-12-23 00:45:41 +0800

    adding sample scripts for the session 15

--- s15/ai2023_s15_00_02.py
+++ s15/ai2023_s15_00_02.py
@@ -1,7 +1,7 @@
 #!/usr/pkg/bin/python3.10
 
 #
-# Time-stamp: <2023/12/21 18:12:48 (Taiwan_Standard_Time_UT+8) daisuke>
+# Time-stamp: <2023/12/23 00:43:51 (Taiwan_Standard_Time_UT+8) daisuke>
 #
 
 # importing numpy module
@@ -19,7 +19,7 @@ import matplotlib.backends.backend_agg
 file_data = 'ai2023_s15_00_00.data'
 
 # figure file
-file_fig = 'ai2023_s15_00_02.pdf'
+file_fig = 'ai2023_s15_00_02.png'
 
 # making empty lists for storing data
 list_a_x               = []
