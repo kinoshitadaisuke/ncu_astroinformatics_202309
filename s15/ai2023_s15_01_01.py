08dd0062a3cc7c74448d7337e2b90163acfadc95
kinoshitadaisuke <mhdf23888@gmail.com>
2023-12-22 22:00:07 +0800

    adding sample scripts for the session 15

---
+++ s15/ai2023_s15_01_01.py
@@ -0,0 +1,81 @@
+#!/usr/pkg/bin/python3.10
+
+#
+# Time-stamp: <2023/12/21 19:49:58 (Taiwan_Standard_Time_UT+8) daisuke>
+#
+
+# importing numpy module
+import numpy
+
+# importing matplotlib
+import matplotlib.figure
+import matplotlib.backends.backend_agg
+
+# data file
+file_data = 'ai2023_s15_01_00.data'
+
+# figure file
+file_fig = 'ai2023_s15_01_01.png'
+
+# lists for storing data
+list_a_x = []
+list_a_y = []
+list_b_x = []
+list_b_y = []
+
+# opening data file
+with open (file_data, 'r') as fh:
+    # reading data line-by-line
+    for line in fh:
+        # stripping line feed at the end of line
+        line = line.strip ()
+        # skipping line if the line starts with '#'
+        if (line[0] == '#'):
+            continue
+        # splitting line into fields
+        (x_str, y_str, group) = line.split ()
+        # converting string into float
+        x = float (x_str)
+        y = float (y_str)
+        # appending data to lists
+        if (group == 'A'):
+            list_a_x.append (x)
+            list_a_y.append (y)
+        elif (group == 'B'):
+            list_b_x.append (x)
+            list_b_y.append (y)
+
+# making numpy arrays
+array_a_x = numpy.array (list_a_x)
+array_a_y = numpy.array (list_a_y)
+array_b_x = numpy.array (list_b_x)
+array_b_y = numpy.array (list_b_y)
+
+# making objects "fig", "canvas", and "ax"
+fig    = matplotlib.figure.Figure ()
+canvas = matplotlib.backends.backend_agg.FigureCanvasAgg (fig)
+ax     = fig.add_subplot (111)
+
+# labels
+ax.set_xlabel ('Feature X [arbitrary unit]')
+ax.set_ylabel ('Feature Y [arbitrary unit]')
+
+# axes
+ax.grid ()
+
+# plotting data
+ax.plot (array_a_x, array_a_y, \
+         linestyle='None', marker='o', markersize=3, color='blue', \
+         label='Known A')
+ax.plot (array_b_x, array_b_y, \
+         linestyle='None', marker='^', markersize=3, color='red', \
+         label='Known B')
+
+# title
+ax.set_title ('Training dataset for classification')
+
+# legend
+ax.legend ()
+
+# saving plot into a file
+fig.savefig (file_fig, dpi=100)
