0de1250d4d3ba4a5e7c3be139a3fd156a9f44ebb
kinoshitadaisuke <mhdf23888@gmail.com>
2023-12-24 17:18:53 +0800

    adding sample scripts for the session 15

---
+++ s15/ai2023_s15_06_00.py
@@ -0,0 +1,42 @@
+#!/usr/pkg/bin/python3.10
+
+#
+# Time-stamp: <2023/12/23 19:03:54 (Taiwan_Standard_Time_UT+8) daisuke>
+#
+
+# importing urllib module
+import urllib.request
+
+# importing ssl module
+import ssl
+
+# allow insecure downloading
+ssl._create_default_https_context = ssl._create_unverified_context
+
+# URL of data file
+url_data = 'https://sbnarchive.psi.edu/pds4/non_mission/compil.ast.ubv-photometry.zip'
+
+# output file name
+file_output = 'colour_ubv.zip'
+
+# printing status
+print (f'Now, fetching {url_data}...')
+
+# opening URL
+with urllib.request.urlopen (url_data) as fh_read:
+    # reading data
+    data_byte = fh_read.read ()
+
+# printing status
+print (f'Finished fetching {url_data}!')
+
+# printing status
+print (f'Now, writing data into file "{file_output}"...')
+
+# opening file for writing
+with open (file_output, 'wb') as fh_write:
+    # writing data
+    fh_write.write (data_byte)
+
+# printing status
+print (f'Finished writing data into file "{file_output}"!')
